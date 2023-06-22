ad7af29baf6e7b9b4035548de92ca3b51dd7a521
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-06-23 02:37:03 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./*/*.{html,js, jsx, ts, tsx, md, mdx}",
+		"./src/*/*.{html,js, jsx, ts, tsx, md, mdx}",
+		"./*.html",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
