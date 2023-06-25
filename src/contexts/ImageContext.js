cd5f5f47ef4473a31fb2382b78cbcc28023ff8be
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-06-25 21:54:34 +0530

    images now match the quote

--- src/contexts/ImageContext.js
+++ src/contexts/ImageContext.js
@@ -8,28 +8,22 @@ const pixabay_api_key = "18846369-871c95044dd7c0f31f2b303e0";
 const unsplash_api_key = "UBAXKPq5WgOxU9z7XjLZrt8B0hayG2dv8gh_vyGQg-0";
 const pexels_api_key =
 	"RfvKt0kQLXP7affcVkFgHWHcsRSD9CGKz91wOPBeLf7suLFEJJdjSWv3";
-const client = createClient(pexels_api_key);
-
+let d = [];
 const ImageContextProvider = (props) => {
 	const [image, setImages] = useState("");
 
 	const fetchImages = async (setIsLoading, queries) => {
 		setIsLoading(true);
 		console.log(queries);
-		let query = "nature";
-		try {
-			let query = [...queries];
-			query = query.join("+");
-            // if the query contains "famous quotes" change it to nature.
-            if (query.includes("famous quotes")) {
-				query = "nature";
-			}
-			console.log(query);
-		} catch (error) {
-			console.log(error);
-			query = "nature";
-		}
+        let query = [...queries];
+        query = query.join("+");
+        // if the query contains "famous quotes" change it to nature.
+        if (query.includes("Famous Quotes")) {
+            query = "nature";
+        }
 
+        
+        console.log(query);
 		// fetching images
 		const pixabay_response = await axios.get(
 			`https://pixabay.com/api/?key=${pixabay_api_key}&q=${query}&image_type=photo&pretty=true&per_page=200`
@@ -40,23 +34,31 @@ const ImageContextProvider = (props) => {
 		const unsplash_response = await axios.get(
 			`https://api.unsplash.com/search/photos/?query=${query}&client_id=${unsplash_api_key}&per_page=${numberOfPhotos}`
 		);
+        const client = createClient(pexels_api_key);
 
 		const pexels_response = await client.photos.search({
 			query,
 			per_page: 80,
-		});
+        });
+        const data3 = {
+            pixabay: pixabay_response.data.hits,
+            unsplash: unsplash_response.data.results,
+            pexels: pexels_response.photos
+        }
+        console.log(data3)
 
         let pix = pixabay_response.data.hits.map(
 			(image) => image.largeImageURL
         );
         let uns = unsplash_response.data.results.map(
-            (image) => image.urls.full
+            (image) => image.urls.regular
         );
         let pex = pexels_response.photos.map(
             (image) => image.src.large2x
         );
 
         const data = [...pix, ...uns, ...pex];
+        d = data;
         console.log(data.length);
 
         // select one image at random:
@@ -69,7 +71,9 @@ const ImageContextProvider = (props) => {
 		<div>
 			<imageContext.Provider value={{ image, fetchImages }}>
 				{props.children}
-			</imageContext.Provider>
+            </imageContext.Provider>
+            {d.map((image) => <img src={image} width={"200px"} alt="" />)}
+
 		</div>
 	);
 };
